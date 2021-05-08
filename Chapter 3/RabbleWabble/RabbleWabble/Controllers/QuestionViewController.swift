e7d87cc941a4024c66a7020497047b4822c666b0
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-08 23:24:27 +0300

    init: start third chapter

---
+++ Chapter 3/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RabbleWabble
+//
+//  Created by Egor Gorskikh on 08.05.2021.
+//
+
+import UIKit
+
+public class QuestionViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
