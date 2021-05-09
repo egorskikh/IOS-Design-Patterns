618fd4560488e71663dfe9687e3d8efdae8590a7
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-09 16:06:23 +0300

    feat: completed MVC chapters

---
+++ 1. MVC/RabbleWabble/RabbleWabble/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Egor Gorskikh on 09.05.2021.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ 1. MVC/RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Egor Gorskikh on 09.05.2021.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
